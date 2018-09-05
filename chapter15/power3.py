2f2be42f6e287dc1d250efa237b46a1da2e4a8c4
liuyiqiang <liuyiqiang@kyeegroup.com>
2018-09-05 16:13:34 +0800

    commit chapter15

---
+++ chapter15/power3.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+
+L=[1,2,4,8,16,32,64]
+X=5
+
+if 2**X in L:
+    print('at index',L.index(2**X))
+else:
+    print(X,'not found')
---
+++ chapter15/power4.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+L=[]
+for i in range(7):
+    L.append(2**i)
+L=list(map(lambda x:2**x,range(7)))
+X=5
+
+if 2**X in L:
+    print('at index',L.index(2**X))
+else:
+    print(X,'not found')
