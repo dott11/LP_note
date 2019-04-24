61557028db390388fe2113dd03dbcac75a76e01b
liuyiqiang <liuyiqiang@kyeegroup.com>
2019-04-24 14:48:49 +0800

    commit chapter32

---
+++ chapter32/except1.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+
+try:
+#     raise IndexError
+    print("try no except")
+except IndexError:
+    print("got exception")
+else:
+    print("else")
+finally:
+    print("finally")
+print("out of try")
+
+try no except
+else
+finally
+out of try
